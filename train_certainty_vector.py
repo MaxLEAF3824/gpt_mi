fix bugs of attention mask

train_certainty_vector.py
@@ -120,8 +120,8 @@ def train_certainty_vector(
     v_c.to(model.cfg.dtype).to(model.cfg.device)
 
     print("Running get_num_tokens")
-    train_dst = train_dst.map(partial(get_num_tokens, tokenizer=hf_tokenizer), new_fingerprint=str(time()), batched=True, batch_size=batch_size)
-    val_dst = val_dst.map(partial(get_num_tokens, tokenizer=hf_tokenizer), new_fingerprint=str(time()), batched=True, batch_size=batch_size)
+    train_dst = train_dst.map(get_num_tokens, new_fingerprint=str(time()), batched=True, batch_size=batch_size)
+    val_dst = val_dst.map(get_num_tokens, new_fingerprint=str(time()), batched=True, batch_size=batch_size)
 
     # setup optimizer
     optimizer = torch.optim.Adam(v_c.parameters(), lr=lr)
@@ -140,7 +140,10 @@ def train_certainty_vector(
         else:
             raise ValueError(f"label_type {label_type} not supported")
         batch_labels = torch.tensor(batch_labels, dtype=batch_scores.dtype).to(batch_scores.device)
-        return F.mse_loss(batch_scores, batch_labels)
+        loss = F.mse_loss(batch_scores, batch_labels)
+        if loss.isnan():
+            print(f"batch_scores:{batch_scores}")
+        return loss
 
     def eval_func(scores, labels):
         discrete_labels = [1 if l > c_th else 0 for l in labels]
@@ -179,7 +182,7 @@ def train_certainty_vector(
                     optimizer.step()
                     optimizer.zero_grad()
                     wandb.log({f'train_step_loss': loss.item()})
-                    
+                
                 epoch_loss.append(loss.item())
                 epoch_scores.extend(batch_scores.tolist())
 

utils.py
@@ -226,22 +226,26 @@ def _get_answer_prob(input_ids, washed_answer_ids, prob):
 
 def _get_batch_padded_output_ids(batch_input_ids, batch_washed_answer_ids, pad_token_id, padding_side):
     batch_output_ids = [torch.tensor(inp + list(ans), dtype=torch.long) for inp, ans in zip(batch_input_ids, batch_washed_answer_ids)]
+    batch_attention_mask = [torch.ones(len(output_ids), dtype=torch.long) for output_ids in batch_output_ids]
     max_len = max(map(len, batch_output_ids))
     if padding_side == "right":
         padded_output_ids = torch.cat([F.pad(output_ids, (0, max_len - len(output_ids)), value=pad_token_id).unsqueeze(0) for output_ids in batch_output_ids], dim=0)
+        attention_mask = torch.cat([F.pad(torch.tensor(mask, dtype=torch.long), (0, max_len - len(mask)), value=0).unsqueeze(0) for mask in batch_attention_mask], dim=0)
     elif padding_side == "left":
         padded_output_ids = torch.cat([F.pad(output_ids, (max_len - len(output_ids), 0), value=pad_token_id).unsqueeze(0) for output_ids in batch_output_ids], dim=0)
+        attention_mask = torch.cat([F.pad(torch.tensor(mask, dtype=torch.long), (max_len - len(mask), 0), value=0).unsqueeze(0) for mask in batch_attention_mask], dim=0)
     else:
         raise ValueError(f"padding_side {padding_side} not supported")
-    return padded_output_ids
+
+    return padded_output_ids, attention_mask
 
 
 def get_answer_prob(examples, model):
     bsz = len(examples['input'])
     batch_answer_prob = []
-    padded_output_ids = _get_batch_padded_output_ids(examples['input_ids'], examples['washed_answer_ids'], model.tokenizer.pad_token_id, 'right')
+    padded_output_ids, attention_mask = _get_batch_padded_output_ids(examples['input_ids'], examples['washed_answer_ids'], model.tokenizer.pad_token_id, 'right')
     with Timer() as timer:
-        batch_prob = F.softmax(model(padded_output_ids, prepend_bos=False), dim=-1)  # prob: (bsz pos vocab)
+        batch_prob = F.softmax(model(padded_output_ids, attention_mask=attention_mask, prepend_bos=False), dim=-1)  # prob: (bsz pos vocab)
     for i in range(bsz):
         batch_answer_prob.append(_get_answer_prob(examples['input_ids'][i], examples['washed_answer_ids'][i], batch_prob[i]))
 
@@ -254,8 +258,8 @@ def get_sampled_answer_prob(example, model):
     batch_answer_prob = []
     washed_sampled_answer_ids = [tuple(i) for i in example['washed_sampled_answer_ids']]
     washed_sampled_answer_ids_unique = list(set(washed_sampled_answer_ids))
-    padded_output_ids = _get_batch_padded_output_ids([example['input_ids']] * len(washed_sampled_answer_ids_unique), washed_sampled_answer_ids_unique, model.tokenizer.pad_token_id, 'right')
-    batch_prob = F.softmax(model(padded_output_ids, prepend_bos=False), dim=-1)  # logits: (bsz pos vocab)
+    padded_output_ids, attention_mask = _get_batch_padded_output_ids([example['input_ids']] * len(washed_sampled_answer_ids_unique), washed_sampled_answer_ids_unique, model.tokenizer.pad_token_id, 'right')
+    batch_prob = F.softmax(model(padded_output_ids, attention_mask=attention_mask, prepend_bos=False), dim=-1)  # logits: (bsz pos vocab)
 
     for i in range(len(washed_sampled_answer_ids)):
         prob = batch_prob[washed_sampled_answer_ids_unique.index(washed_sampled_answer_ids[i])]
@@ -453,8 +457,8 @@ def ours_forward_func(examples, v_c, score_func, model):
 
     full_act_names = [k.replace('#', '.') for k in v_c.keys()]
     fwd_hooks = [(lambda x: x in full_act_names, score_hook)]
-    padded_output_ids = _get_batch_padded_output_ids(examples['input_ids'], examples['washed_answer_ids'], model.tokenizer.pad_token_id, 'left')
-    out = model.run_with_hooks(padded_output_ids, fwd_hooks=fwd_hooks, prepend_bos=False)
+    padded_output_ids, attention_mask = _get_batch_padded_output_ids(examples['input_ids'], examples['washed_answer_ids'], model.tokenizer.pad_token_id, 'left')
+    out = model.run_with_hooks(padded_output_ids, fwd_hooks=fwd_hooks, attention_mask=attention_mask, prepend_bos=False)
 
     batch_scores = einops.reduce(layer_batch_scores, 'l b -> b', 'mean')
     return batch_scores